42c7fddc0196b367dad63ac989a44566361be773
Shankar Pilli <shankar.pilli@sparity.com>
2017-04-25 17:00:22 +0530

    First commit

---
+++ app/src/main/java/com/magni5/m5tracker/MainActivity.java
@@ -0,0 +1,13 @@
+package com.magni5.m5tracker;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
