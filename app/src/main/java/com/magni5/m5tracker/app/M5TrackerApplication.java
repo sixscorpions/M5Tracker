227aed185afa58d79c5b1b1e8f1fee5ee832f645
Shankar Pilli <shankar.pilli@sparity.com>
2017-04-28 11:41:47 +0530

    Added M5 Tracker Maps Fragment issue

---
+++ app/src/main/java/com/magni5/m5tracker/app/M5TrackerApplication.java
@@ -0,0 +1,15 @@
+package com.magni5.m5tracker.app;
+
+import android.support.multidex.MultiDexApplication;
+
+/**
+ * Created by Shankar on 4/28/2017.
+ */
+
+public class M5TrackerApplication extends MultiDexApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
