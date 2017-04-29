de057b5ba9716ccb7e7a46d5b7b06234191f2631
shankar <shankar.pilli@sparity.com>
2017-04-29 18:02:40 +0530

    Changed lancher icon and decreased splash time

--- app/src/main/java/com/magni5/m5tracker/utils/Constants.java
+++ app/src/main/java/com/magni5/m5tracker/utils/Constants.java
@@ -9,7 +9,7 @@ public class Constants {
     // Log message On or Off
     public static final boolean logMessageOnOrOff = true;
     public static final String IMAGES_FOLDER = "Images/";
-    public static int SPLASH_TIME_OUT = 2000;
+    public static int SPLASH_TIME_OUT = 1500;
 
     public static final String APP_PREF = "M5Tracker_Pref";
     public static final String LOGIN_SESSION_ID = "login_session_id";
